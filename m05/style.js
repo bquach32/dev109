5c81c8e3bdf344718cf43f7f11cbbaaec6cbcf8c
bquach32 <buuquach32@gmail.com>
2025-02-05 15:43:53 -0800

    Create style.js

---
+++ m05/style.js
@@ -0,0 +1,6 @@
+var msg = "<h2>Multiplication Table</h2>"
+
+var table = prompt("Please enter your name", "Buu Quach");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
