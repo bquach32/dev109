803dad01d64400aa5a09c77793d820aadc47254a
bquach32 <buuquach32@gmail.com>
2025-02-05 15:54:48 -0800

    Update style.js

--- m05/style.js
+++ m05/style.js
@@ -1,6 +1,26 @@
-var msg = "<h2>Multiplication Table</h2>"
-
 var table = prompt("Please enter your name", "Buu Quach");
   if (person != null) {
     document.getElementById("demo").innerHTML =
-    "Hello " + person + "! How are you today?";
+    "Hello " + person + "! How are you today?";             // Unit of table
+
+var operator = 'addition'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
+
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' * ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
