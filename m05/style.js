0a65941b4d92c31816337840926d6a3fa26d7cbb
bquach32 <buuquach32@gmail.com>
2025-02-06 19:52:08 -0800

    Update style.js

--- m05/style.js
+++ m05/style.js
@@ -1,25 +1,37 @@
-let number = prompt ("What is 1 multiplied by 5?");
+let number = prompt("What is 1 multiplied by 5?");
 
-var table = 5;             // Unit of table
-var operator = 'Multiplication'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';              // Message
+var table = 5;             
+var operator = 'Multiplication'; 
+var i = 1;                 
+var msg = '<h2>Multiplication Table</h2>';           
 
 if (operator === 'addition') {
-  // Do addition
-  while (i < 10) {
+  while (i <= 10) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
 } else {
-  // Do multiplication
-  while (i < 10) {
+  while (i <= 10) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
 }
 
+var el = document.getElementById('blackboard');
+if (el) {
+  el.innerHTML = msg;  
+} else {
+  console.error("Element with id 'blackboard' not found.");
+}
+
+if (parseInt(number) === 5) {
+  alert("Correct!");
+} else {
+  alert("Try again.");
+}
+
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg + GetTableContent(table);
+
 
