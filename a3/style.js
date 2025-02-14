b94f4a6d4cd0d9b319fb58c7218d0ac7e849fa6a
bquach32 <buuquach32@gmail.com>
2025-02-14 13:09:45 -0800

    Update style.js

--- a3/style.js
+++ a3/style.js
@@ -10,71 +10,29 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
   downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
-function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-  let rhombusHTML = "";
-  
-  rhombusHTML += createUpLeft(pHeight, pColorEven, pColorOdd, pSymbol);
-  rhombusHTML += createUpRight(pHeight, pColorEven, pColorOdd, pSymbol);
-  rhombusHTML += createDownLeft(pHeight, pColorEven, pColorOdd, pSymbol);
-  rhombusHTML += createDownRight(pHeight, pColorEven, pColorOdd, pSymbol);
+function printDiamond(n) {
+        var space = n - 1;
+        for (var i = 0; i < n; i++) {
+     
+          for (var j = 0; j < space; j++) document.write("&nbsp;&nbsp;");
 
-  document.getElementById("rhombus").innerHTML = rhombusHTML;
-}
+          // Print i+1 stars
+          for (var j = 0; j <= i; j++) document.write("*" + "&nbsp;&nbsp;");
 
-function createUpLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-  let rLine = "";
-  for (let i = 0; i < pHeight; i++) {
-      rLine += "<p>";
-      for (let s = 0; s < pHeight - i; s++) {
-          rLine += "<span style='visibility:hidden'>" + pSymbol + "</span>";
-      }
-      for (let j = 0; j <= i; j++) {
-          let color = j % 2 === 0 ? pColorOdd : pColorEven;
-          rLine += "<span style='color:" + color + ";'>" + pSymbol + "</span>";
-      }
-      rLine += "</p>";
-  }
-  return rLine;
-}
+          document.write("<br>");
+          space--;
+        }
 
-function createUpRight(pHeight, pColorEven, pColorOdd, pSymbol) {
-  let rLine = "";
-  for (let i = 0; i < pHeight; i++) {
-      rLine += "<p>";
-      for (let j = 0; j <= i; j++) {
-          let color = j % 2 === 0 ? pColorOdd : pColorEven;
-          rLine += "<span style='color:" + color + ";'>" + pSymbol + "</span>";
-      }
-      rLine += "</p>";
-  }
-  return rLine;
-}
+        space = 0;
 
-function createDownLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-  let rLine = "";
-  for (let i = pHeight - 1; i >= 0; i--) {
-      rLine += "<p>";
-      for (let s = 0; s < pHeight - i; s++) {
-          rLine += "<span style='visibility:hidden'>" + pSymbol + "</span>";
-      }
-      for (let j = 0; j <= i; j++) {
-          let color = j % 2 === 0 ? pColorOdd : pColorEven;
-          rLine += "<span style='color:" + color + ";'>" + pSymbol + "</span>";
-      }
-      rLine += "</p>";
-  }
-  return rLine;
-}
+        for (var i = n; i > 0; i--) 
+        {
+   
+          for (var j = 0; j < space; j++) document.write("&nbsp;&nbsp;");
 
-function createDownRight(pHeight, pColorEven, pColorOdd, pSymbol) {
-  let rLine = "";
-  for (let i = pHeight - 1; i >= 0; i--) {
-      rLine += "<p>";
-      for (let j = 0; j <= i; j++) {
-          let color = j % 2 === 0 ? pColorOdd : pColorEven;
-          rLine += "<span style='color:" + color + ";'>" + pSymbol + "</span>";
+          for (var j = 0; j < i; j++) document.write("*" + "&nbsp;&nbsp;");
+
+          document.write("<br>");
+          space++;
+        }
       }
-      rLine += "</p>";
-  }
-  return rLine;
-}
