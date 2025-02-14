732753604cd8c7e5aaf310dab650c113eefe74a1
bquach32 <buuquach32@gmail.com>
2025-02-14 03:41:15 -0800

    Update style.js

--- a3/style.js
+++ a3/style.js
@@ -10,6 +10,13 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
   downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+  upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+  downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+  upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+  downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
 function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
   var rLine = "";
   for (var i = 0; i < pHeight; i++) {
@@ -22,8 +29,10 @@ function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
     for (var j = 0; j <= i; j++) {
       // Is the position even or odd so we change the color
       if (j % 2 === 0) {
+        // odd
         rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
       } else {
+        // even
         rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
       }
     }
@@ -32,6 +41,7 @@ function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
 
   document.getElementById("upLeft").innerHTML = rLine;
 } 
+
 
 function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
   var rLine = "";
@@ -43,9 +53,12 @@ function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
     }
     // Create each line on the Rhombus
     for (var j = 0; j < i; j++) {
+      // Is the position even or odd so we change the color
       if (j % 2 === 0) {
+        // odd
         rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
       } else {
+        // even
         rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
       }
     }
@@ -54,50 +67,44 @@ function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
 
   document.getElementById("downLeft").innerHTML = rLine;
 }
-function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=0;i<pHeight;i++){
-rLine +="<p>";
-//Create each line on the Rhombus
-for(j=0;j<=i;j++){
 
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (var i = 0; i < pHeight; i++) {
+    rLine += "<p>";
+    // Create each line on the Rhombus
+    for (var j = 0; j <= i; j++) {
+      // Is the position even or odd so we change the color
+      if (j % 2 === 0) {
+        // odd
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+      } else {
+        // even
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      }
+    }
+    rLine += "</p>";
+  }
 
-}
-rLine +="</p>";
-// console.log(rLine);
+  document.getElementById("upRight").innerHTML = rLine;
+} 
 
-}
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (var i = pHeight; i > 0; i--) {
+    rLine += "<p>";
+    // Create each line on the Rhombus
+    for (var j = 0; j < i; j++) {
+      // Is the position even or odd so we change the color
+      if (j % 2 === 0) {
+        // odd
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+      } else {
+        // even
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      }
+    }
+    rLine += "</p>";
+  }
 
-document.getElementById("upRight").innerHTML = rLine;
-}
-
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=pHeight;i > 0;i--){
-rLine +="<p>";
-//Create each line on the Rhombus
-for(j=0;j<i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("downRight").innerHTML = rLine;
-}
+  document.getElementById("downRight").innerHTML = rLine;
