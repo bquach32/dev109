097cfc2a6a2013c47f2e404fe637b857da4a5a51
bquach32 <buuquach32@gmail.com>
2025-02-14 02:44:04 -0800

    Update style.js

--- a3/style.js
+++ a3/style.js
@@ -13,10 +13,10 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
-for (i = 0; i<pHeight; i++){
+for (var i = 0; i<pHeight; i++){
 rLine +="<p>";
 //Create each line on the Rhombus
-for(j=0;j<=i;j++){
+for(var j=0;j<=i;j++){
 
 //Is the position even or odd so we change the color
 if (j%2)
@@ -37,10 +37,10 @@ document.getElementById("upRight").innerHTML = rLine;
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
-for (i=pHeight;i > 0;i--){
+for (var i=pHeight;i > 0;i--){
 rLine +="<p>";
 //Create each line on the Rhombus
-for(j=0;j<i;j++){
+for(var j=0;j<i;j++){
 
 //Is the position even or odd so we change the color
 if (j%2)
@@ -61,23 +61,24 @@ document.getElementById("downRight").innerHTML = rLine;
 
 function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
-for (i = 0; i < pHeight; i++){
+for (var i = 0; i < pHeight; i++){
 rLine +="<p>";
-//Create each line on the Rhombus
-for(j = 0; j <= i; j++){
 
-//Is the position even or odd so we change the color
+     for (var s = 0; s < pHeight - i; s++) {
+      rLine += "<span style='visibility:hidden'>" + pSymbol + "</span>";
+       
+for(var j = 0; j <= i; j++){
+
+
 if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
+
 rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+else
+
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
 
 }
 rLine +="</p>";
-// console.log(rLine);
-
 }
 
 document.getElementById("upLeft").innerHTML = rLine;
@@ -85,23 +86,24 @@ document.getElementById("upLeft").innerHTML = rLine;
 
 function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
-for (i=pHeight; i > 0; i--){
+for (var i=pHeight; i > 0; i--){
 rLine += "<p>";
-//Create each line on the Rhombus
-for(j = 0; j < i; j++){
 
-//Is the position even or odd so we change the color
+    for (var s = 0; s < pHeight - i; s++) {
+      rLine += "<span style='visibility:hidden'>" + pSymbol + "</span>";
+    }
+  
+for(var j = 0; j < i; j++){
+
 if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
+
 rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+else
+
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
 
 }
 rLine +="</p>";
-// console.log(rLine);
-
 }
 
 document.getElementById("downLeft").innerHTML = rLine;
