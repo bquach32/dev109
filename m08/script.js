ed2eaa52ad6766772a4b48cb47a92be81d07296d
bquach32 <buuquach32@gmail.com>
2025-02-25 19:41:08 -0800

    Update script.js

--- m08/script.js
+++ m08/script.js
@@ -1,22 +1,35 @@
-var newitem = document.createElement('item');
-if (newitem.trim() === "") {
+// Add the function and the variable (Buu)
+function addItem() {
+  var newitem = document.getElementById('item').value;
+// Make sure a valid item in the input field (Buu)
+  if (newitem.trim() === "") {
     alert("Please enter an item.");
     return;
+  }
 
-// Create a new element and store it in a variable.
-var newEl = document.createElement('li');
+  // Create a new element and store it in a variable.
+  var newEl = document.createElement('li');
+    
+  // Changed "quinoa" to "newitem" (Buu)
+  // Create a text node and store it in a variable.
+  var newText = document.createTextNode(newitem); 
 
-// Create a text node and store it in a variable.
-var newText = document.createTextNode('newitem');
+  // Attach the new text node to the new element.
+  newEl.appendChild(newText);
 
-// Attach the new text node to the new element.
-newEl.appendChild(newText);
+  // Find the position where the new element should be added.
+  var position = document.getElementsByTagName('ul')[0];
 
-// Find the position where the new element should be added.
-var position = document.getElementsByTagName('ul')[0];
+  // Insert the new element into its position.
+  position.appendChild(newEl);
 
-  
-// Insert the new element into its position.
-position.appendChild(newEl);
-  
-document.getElementById('item').value = "";
+  // Clear the input field (Buu)
+  document.getElementById('item').value = '';
+}
+
+  // Added by pressing "Enter" key from keyboard (Buu)
+document.getElementById('item').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    addItem();
+  }
+});
