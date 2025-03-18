530be68afbb8d3f5146da8f0a2d3f97af483aeed
bquach32 <buuquach32@gmail.com>
2025-03-17 20:34:55 -0700

    Update music.js

--- finalp1/js/music.js
+++ finalp1/js/music.js
@@ -1,1 +1,7 @@
-
+window.addEventListener("click", function() {
+    let audio = document.querySelector("audio"); 
+    if (audio) {
+        audio.volume = 0.3; 
+        audio.play(); 
+    }
+});
