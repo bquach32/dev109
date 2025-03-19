50b0b45f64f0b88d427bc95b254941718e6fb7d7
bquach32 <buuquach32@gmail.com>
2025-03-19 15:13:45 -0700

    Update music.js

--- finalp1/js/music.js
+++ finalp1/js/music.js
@@ -1,14 +1,14 @@
-        // Function to play the background music with volume control
-        function playBackgroundMusic() {
-            var music = document.getElementById("music"); 
-            music.volume = 0.3; 
-            music.play().catch(function(error) { 
-                console.log("Autoplay was prevented: " + error); 
-            });
-        }
 
-        $(document).ready(function() {
-            $("body").one("click", function() {
-                playBackgroundMusic();
-            });
-        });
+function playBackgroundMusic() {
+    var music = document.getElementById("music"); 
+    music.volume = 0.3;
+    music.play().catch(function(error) { 
+        console.log("Autoplay was prevented: " + error); 
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener("click", function() {
+        playBackgroundMusic();
+    }, { once: true }); 
+});
