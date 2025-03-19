e823d1ed4e2ec23f0d4c69103e8e703919410748
bquach32 <buuquach32@gmail.com>
2025-03-19 14:59:59 -0700

    Update music.js

--- finalp1/js/music.js
+++ finalp1/js/music.js
@@ -1,8 +1,6 @@
  $(document).ready(function() {
-            // Ensure the audio is ready to play
             var play = document.getElementById("music");
-            
-            // Play music when the user clicks anywhere on the page
+  
             $("body").one("click", function() {
                 play.play().catch(function(error) {
                     console.log("Autoplay prevented: " + error);
